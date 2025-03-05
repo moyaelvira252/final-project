5784334835b866d35eff1b70e3f354fb85c80346
moyaelvira252 <moyaelvira252@gmail.com>
2025-03-05 19:44:56 +0000

    Add 1.js

---
+++ src/1.js
@@ -0,0 +1,12 @@
+<script>
+function getRandomCode() {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 8; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+console.log(getRandomCode());
+</script>
