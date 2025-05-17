4ed578dfdaf93101a3cc1f65a9e282beab3860b4
moyaelvira252 <moyaelvira252@gmail.com>
2025-05-17 04:11:51 +0000

    Add 36.js

---
+++ src/36.js
@@ -0,0 +1,7 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// Example usage
+const result = getRandomInt(100); // This will print a random integer between 0 and 99
+console.log(result);
